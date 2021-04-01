c5eface674c1fdbfff0c845e68f05b9ee1ed6ca3
Cyrus Goh <hello@lovincyrus.com>
2021-04-01 16:11:00 -0700

    add v-scrollto for tm-link

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   css: [{ src: '~/assets/styles/main.styl', lang: 'stylus' }],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/vue-scrollto.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
